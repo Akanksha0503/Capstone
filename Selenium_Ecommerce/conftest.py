09611257543890fbb6f7a187c705ae10fe589a3c
Akanksha Singh <akankshasingh050304@gmail.com>
2025-10-19 10:58:36 +0530

    commit with Capstone Project

--- Selenium_Ecommerce/conftest.py
+++ Selenium_Ecommerce/conftest.py
@@ -83,7 +83,7 @@ def setup(request):
         options.add_argument("--width=1920")
         options.add_argument("--height=1080")
         options.add_argument("--disable-gpu")
-        user_data_dir = tempfile.mkdtemp()
+        user_data_dir = tempfile.mkdtemp()  # create a unique temp directory
         options.add_argument(f"--user-data-dir={user_data_dir}")
         if headless:
             options.add_argument("--headless")
@@ -137,7 +137,6 @@ def setup(request):
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
-    shutil.rmtree(user_data_dir, ignore_errors=True)
 
 # ------------------------------------------------------------------
 #  Login Fixture
