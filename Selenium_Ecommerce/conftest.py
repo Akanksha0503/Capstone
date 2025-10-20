01538b0ddabdef28021f11f4b79a1b73f2b951be
Akanksha Singh <akankshasingh050304@gmail.com>
2025-10-20 11:32:58 +0530

    commit with Capstone Project

--- Selenium_Ecommerce/conftest.py
+++ Selenium_Ecommerce/conftest.py
@@ -30,9 +30,8 @@ GRID_URL = "http://localhost:4444/wd/hub"
 # Set GitHub token to avoid API rate limits when downloading GeckoDriver
 #WDM_GITHUB_TOKEN = "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"
 import os
-token = os.getenv("WDM_GITHUB_TOKEN")
-if token:
-    os.environ["WDM_GITHUB_TOKEN"] = token
+os.environ.get("WDM_GITHUB_TOKEN")  # Used by webdriver_manager
+
 
 def pytest_addoption(parser):
     parser.addoption(
