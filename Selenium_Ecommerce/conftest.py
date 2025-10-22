cc768520db28c9c3295f048e00ed65649e3f0a5f
Akanksha Singh <akankshasingh050304@gmail.com>
2025-10-22 12:12:06 +0530

    Update Yml file

--- Selenium_Ecommerce/conftest.py
+++ Selenium_Ecommerce/conftest.py
@@ -188,19 +188,4 @@ def setup(request):
     yield driver
     driver.quit()
 # Attach screenshots on failure to Allure report
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
-    if rep.when == 'call' and rep.failed:
-        driver = item.funcargs.get('driver') if hasattr(item, 'funcargs') else None
-        if not driver:
-            # Try alternative access
-            driver = item._request.funcargs.get('driver') if hasattr(item, '_request') else None
-        if driver:
-            try:
-                png = driver.get_screenshot_as_png()
-                allure.attach(png, name='screenshot', attachment_type=allure.attachment_type.PNG)
-            except Exception:
-                # don't raise in hook
-                pass
+
